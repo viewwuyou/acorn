7bef9473214a78aad2aa2e187a66db6b3916e4f9
viewwuyou <viewwuyou@gmail.com>
2022-10-06 14:21:05 +0800

    init project

---
+++ acorn-start/src/main/java/com/viewwuyou/acorn/web/BaseController.java
@@ -0,0 +1,12 @@
+package com.viewwuyou.acorn.web;
+
+import com.viewwuyou.view.common.Result;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author viewwuyou
+ */
+@RestController
+public class BaseController {
+
+}
